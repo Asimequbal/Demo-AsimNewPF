a3579c895ab92513a82eb61267713553ddf48643
AsimEqubal <asimequbal@gmail.com>
2024-01-05 11:05:19 +0530

    1st page animation

--- script.js
+++ script.js
@@ -3,9 +3,33 @@ const scroll = new LocomotiveScroll({
     smooth: true,
 });
 
+function animatefpage(){
+  var tl = gsap.timeline();
+  tl.from("#nav" , {
+    y: '-10',
+    opacity: 0,
+    duration:1.5,
+    ease: Expo.easeInOut,
+  })
+  .to(".boundelem" , {
+    y: '0',
+    ease: Expo.easeInOut,
+    duration: 1.5,
+    delay: -1,
+    stagger: .2,
+  })
+  .from("#hero-ffoter" , {
+    y: '-10',
+    opacity: 0,
+    duration:1.5,
+    delay: -1,
+    ease: Expo.easeInOut,
+  })
+}
 function moovescrolldot(){
     window.addEventListener('mousemove',function(delt){
       document.querySelector('#minicircle').style.transform = `translate(${delt.clientX}px, ${delt.clientY}px)`
     })
 }
 moovescrolldot();
+animatefpage();
